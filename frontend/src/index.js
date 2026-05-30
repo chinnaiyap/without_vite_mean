e84dfe0901596448f69ed48794151e5b6a37ef88
demo <chinnaiyaperiyasamy@gmail.com>
2026-05-30 08:31:23 +0530

    fixed render router issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/without_vite_mean">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
